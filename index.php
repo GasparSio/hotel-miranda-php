09e9dc8453509323b4bbf60140f388ee3c6e8df4
GasparSio <sio.gaspar@gmail.com>
2023-11-18 17:56:16 +0100

    changes on index.php file

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 require 'lib/BladeOne.php';
-use eftec\bladeone\BladeOne;
-$views = 'views'; // Specify the directory where your Blade templates are located.
-$compiledFolder = 'cache'; // Specify the directory where compiled views will be stored.
-$blade = new BladeOne($views, $compiledFolder, BladeOne::MODE_AUTO);
-// Example usage
-echo $blade->run('index');
 
+use eftec\bladeone\BladeOne;
+
+$views = 'views'; // Directorio donde se encuentran tus plantillas Blade.
+$compiledFolder = 'cache'; // Directorio donde se almacenarán las vistas compiladas.
+
+$blade = new BladeOne($views, $compiledFolder, BladeOne::MODE_AUTO);
+
+// Pasar datos a la vista si es necesario
+$data = [
+    'title' => 'Mi Sitio',
+];
+
+// Renderiza la vista principal (index.blade.php)
+echo $blade->run('index', $data);
 ?>
 
