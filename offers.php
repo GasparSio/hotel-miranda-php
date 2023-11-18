98421ce94ea5c43e9ff65a65f83f5d2dbe957ca0
GasparSio <sio.gaspar@gmail.com>
2023-11-18 18:03:10 +0100

    changes on files root .php

---
+++ offers.php
@@ -0,0 +1,18 @@
+<?php
+require 'lib/BladeOne.php';
+
+use eftec\bladeone\BladeOne;
+
+$views = 'views'; // Directorio donde se encuentran tus plantillas Blade.
+$compiledFolder = 'cache'; // Directorio donde se almacenarán las vistas compiladas.
+
+$blade = new BladeOne($views, $compiledFolder, BladeOne::MODE_AUTO);
+
+// Pasar datos a la vista si es necesario
+$data = [
+    'title' => 'Offers',
+];
+
+// Renderiza la vista principal (index.blade.php)
+echo $blade->run('offers', $data);
+?>
