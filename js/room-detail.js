2b96ae8b526887e6304585b66556b838ba2253ec
GasparSio <sio.gaspar@gmail.com>
2023-11-22 14:47:13 +0100

    changes on room detail form and visual

--- js/room-detail.js
+++ js/room-detail.js
@@ -1,22 +1,22 @@
 //SLIDER FOOD
-const cardsContainer2 = document.querySelector(".room__detail-cards-container");
-const prevBtn3 = document.querySelector(".prev-btn-detail");
-const nextBtn3 = document.querySelector(".next-btn-detail");
+const cardsContainerDetail = document.querySelector(".room__detail-cards-container");
+const prevBtnDetail = document.querySelector(".prev-btn-detail");
+const nextBtnDetail = document.querySelector(".next-btn-detail");
 
-let cardIndex2 = 0;
-const numCards2 = document.querySelectorAll(".room__detail-rooms-slider").length;
+let cardIndexDetail = 0;
+const numCardsDetail = document.querySelectorAll(".room__detail-rooms-slider").length;
 
 function showCardFood(index) {
-    const cards = document.querySelectorAll(".room__detail-rooms-slider");
-    cardsContainer2.style.transform = `translateX(-${index * 320}px)`;
+  const cards = document.querySelectorAll(".room__detail-rooms-slider");
+  cardsContainerDetail.style.transform = `translateX(-${index * 320}px)`;
 }
 
-prevBtn3.addEventListener("click", () => {
-  cardIndex2 = (cardIndex2 - 1 + numCards2) % numCards2;
-  showCardFood(cardIndex2);
+prevBtnDetail.addEventListener("click", () => {
+  cardIndexDetail = (cardIndexDetail - 1 + numCardsDetail) % numCardsDetail;
+  showCardFood(cardIndexDetail);
 });
 
-nextBtn3.addEventListener("click", () => {
-    cardIndex2 = (cardIndex2 + 1) % numCards2;
-    showCardFood(cardIndex2);
+nextBtnDetail.addEventListener("click", () => {
+  cardIndexDetail = (cardIndexDetail + 1) % numCardsDetail;
+  showCardFood(cardIndexDetail);
 });
